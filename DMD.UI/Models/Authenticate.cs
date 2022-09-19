9c8451791b77dd0099595dc58a06d61612de659b
Zachary Keil <700263511@fvtc.edu>
2022-09-19 16:15:30 -0500

    user

---
+++ DMD.UI/Models/Authenticate.cs
@@ -0,0 +1,20 @@
+﻿using DMD.BL.Models;
+using Microsoft.AspNetCore.Http;
+namespace DMD.UI.Models
+{
+    public static class Authenticate
+    {
+        //public static bool IsAuthenticated(HttpContext context)
+        //{
+        //    if (context.Session.GetObject<User>("user") != null)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
+    }
+}
+

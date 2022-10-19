bdcbf910d5898852a919b01abec0a40e72aa6247
Devon Lozier <300068708@fvtc.edu>
2022-10-19 16:27:05 -0500

    More clarity comments for the foreign key constraints.

--- DMD.DB/ForeignKeys/fkUserId.sql
+++ DMD.DB/ForeignKeys/fkUserId.sql
@@ -2,4 +2,4 @@
 	ADD CONSTRAINT [fkUserId-Characters] -- The name of the constraint ("fkUserId-Characters") - indicating a foreign key constraint of UserId in the Characters table.
 	FOREIGN KEY (UserId) -- the foreign key in the Characters table
 	REFERENCES [tblUser] (Id) -- the table the foreign key is referring to (The primary key of tblUser is the foreign key in Characters).
-GO;
+GO; -- indicates this part is done, you can repeat this and GO; onto the next statement.

819921e396e55ad4c09cc9919644ea4c735e1a78
Jordan Johnson <200052266@fvtc.edu>
2022-10-25 21:43:44 -0500

    Character Lists

--- DMD.BL.Models/Character.cs
+++ DMD.BL.Models/Character.cs
@@ -8,5 +8,23 @@ namespace DMD.BL.Models
 {
     public class Character
     {
+        public List<User> Users { get; set; }
+        public List<Character> Characters { get; set; }
+        public List<Race> Race { get; set; }
+        public List<Class> Classes { get; set; }
+        public List<CharacterLevel> CharacterLevel { get; set; }
+        public List<CharacterWeapon> CharactersWeapon { get; set; }
+        public List<CharacterWeaponTypeProficiency> CharacterWeaponTypeProficiencies { get; set; }
+        public List<CharacterAttacks> CharacterAttacks { get; set; }
+        public List<CharacterArmor> CharacterArmors { get; set; }
+        public List<CharacterSpells> CharacterSpells { get; set; }
+        public List<CharacterSpellCharges> CharacterSpellCharges { get; set; }
+        public List<CharacterSKillProficiency> CharacterSKillProficiencies { get; set; }
+        public List<SpellChargesByLevel> SpellChargesByLevels { get; set; }
+        public List<SpellDamageTypes> SpellDamageTypes { get; set; }
+        public List<CharacterCurrency> CharacterCurrency { get; set; }
+        public List<CharacterLanguages> CharacterLanguages { get; set; }
+        public List<Currency> Currency { get; set; }
+        
     }
 }

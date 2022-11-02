eff173ff4a1626a25982e724bf90d665878b7264
Devon Norman <500162063@fvtc.edu>
2022-11-02 15:25:08 -0500

    Created a PL Test, inserted nuget packages of NUnit, and testing the utArmor Class before creating the others

---
+++ DMD.PL.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DMD.PL.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ DMD.PL.Test/utArmor.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Microsoft.EntityFrameworkCore.Storage;
+using DMD.PL;
+using Assert = NUnit.Framework.Assert; //May need to keep.
+
+namespace DMD.PL.Test
+{
+    [TestClass]
+    public class utArmor
+    {
+        //Protected entities instances for the Test to use
+        protected DMDEntities dc;
+        protected IDbContextTransaction transaction;
+
+        //Initialization Test Method for PL
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            dc = new DMDEntities();                         //Instantiates DMDEntities Data for context for PL.
+            transaction = dc.Database.BeginTransaction();   //Instantiates transaction process for the database.
+        }
+
+        //Test Cleanup Method
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            transaction.Rollback();     //Rollback transactions
+            transaction.Dispose();      //Dispose of the transactions
+            dc.Dispose();               //Dispose the data context from the PL.
+        }
+
+        //Load Test Method
+        [TestMethod]
+        public void LoadTest()
+        {
+            Assert.AreEqual(3, dc.tblArmors.Count());
+        }
+
+        //Insert Test Method
+        [TestMethod]
+        public void InsertTest()
+        {
+            //Creates new instance of table and adds a value for each column for the row to be inserted.
+            tblArmor armor = new tblArmor();
+            armor.Id = Guid.NewGuid();
+            armor.ArmorStyle_Id = Guid.NewGuid();
+            armor.ArmorType_Id = Guid.NewGuid();
+            armor.ArmorClassBonus = 100;
+            armor.MovementPenalty = 100;
+            armor.Cost = 100;
+
+            //Add new row and saves changes
+            dc.tblArmors.Add(armor);
+            int result = dc.SaveChanges();
+
+            //Asserts that the value of result is greater than 0, indicating a proper save.
+            Assert.IsTrue(result > 0);
+        }
+    }
+}

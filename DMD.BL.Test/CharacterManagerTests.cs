ff82c3984f48cbbc3c017f522182d283eae2a5a9
Devon Norman <500162063@fvtc.edu>
2022-11-16 07:41:59 -0600

    backup of code, this is a compelte mess I'm sorry

---
+++ DMD.BL.Test/CharacterManagerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DMD.BL.Models;
+
+namespace DMD.BL.Test
+{
+    [TestClass]
+    public class CharacterManagerTests
+    {
+
+        //Test the ability to load data.
+        [TestMethod]
+        public void LoadTest()
+        {
+
+
+            //old code here if needed
+            //Character character = CharacterManager.Load();
+            //List<CharacterArmor> characterArmor = CharacterManager.Load();
+        }
+    }
+}

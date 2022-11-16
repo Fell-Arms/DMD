15e753f881d96b909f1fc86bc662fe6b015fccb2
Jordan Johnson <200052266@fvtc.edu>
2022-11-15 22:41:54 -0600

    Reworked the dice js. worked on play page

--- DMD.UI/wwwroot/js/diceroll.js
+++ DMD.UI/wwwroot/js/diceroll.js
@@ -1,28 +1,23 @@
-﻿//make a function that rolls one die | 1d6
-function d(y) {
-    //generate a random number between 1 and y
-    return Math.floor(Math.random() * y) + 1;
-}
-//make a function that rolls multiple dice | 2d6
-function XdY(x, y) {
-    let results = [];
-    //call d(y) x times and put in results
-    do {
-        results.push(d(y));
-    }
-    while (results.length < x)
-    //return results
-    return results;
-}
-const poolTest = { 6: 2, 8: 2, 10: 7, 20: 1 };
-// make a function that rolls a pool of dice | 2d6 + 5d10
-function dicePool(obj) {
-    // make the results object
-    let results = {};
-    // call XdY for each entry in the obj
-    for (let [y, x] of Object.entries(obj)) {
-        results[y] = XdY(x, y);
-    };
-    // return results
-    return results;
-}
+﻿let x;
+let y;
+let z;
+let i;
+let j;
+let k;
+
+document.getElementById("rollButton").onclick = function () {
+
+    x = Math.floor(Math.random() * 4) + 1;
+    y = Math.floor(Math.random() * 6) + 1;
+    z = Math.floor(Math.random() * 8) + 1;
+    i = Math.floor(Math.random() * 10) + 1;
+    j = Math.floor(Math.random() * 12) + 1;
+    k = Math.floor(Math.random() * 20) + 1;
+
+    document.getElementById("xlabel").innerHTML = x;
+    document.getElementById("ylabel").innerHTML = y;
+    document.getElementById("zlabel").innerHTML = z;
+    document.getElementById("ilabel").innerHTML = i;
+    document.getElementById("jlabel").innerHTML = j;
+    document.getElementById("klabel").innerHTML = k;
+} 

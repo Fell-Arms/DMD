1194d0efe9322d1deabae6b5e3cdda0abf3f89ad
Devon Norman <500162063@fvtc.edu>
2022-11-16 10:28:43 -0600

    Character now loads, but need to get every other part of the character data to load now

---
+++ DMD.BL.Test/ArmorManagerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DMD.BL.Models;
+
+namespace DMD.BL.Test
+{
+    [TestClass]
+    public class ArmorManagerTests
+    {
+        //Test the ability to load data.
+        [TestMethod]
+        public void LoadTest()
+        {
+            //Run Async Task for Loading.
+            Task.Run(async () =>
+            {
+                var task = await ArmorManager.Load();
+                IEnumerable<Models.Armor> armors = task;
+                Assert.AreEqual(3, armors.ToList().Count);
+            }).GetAwaiter().GetResult();
+        }
+    }
+}
--- DMD.BL.Test/CharacterManagerTests.cs
+++ DMD.BL.Test/CharacterManagerTests.cs
@@ -11,15 +11,29 @@ namespace DMD.BL.Test
     public class CharacterManagerTests
     {
 
-        //Test the ability to load data.
+        //Test the ability to load data in CharacterManager
         [TestMethod]
         public void LoadTest()
         {
-            List<Character> characters = CharacterManager.Load();
+            //Run Async Task for Loading.
+            Task.Run(async () =>
+            {
+                var task = await CharacterManager.Load();
+                IEnumerable<Models.Character> characters = task;
+                Assert.AreEqual(3, characters.ToList().Count);
+            }).GetAwaiter().GetResult();
 
-            //old code here if needed
-            //Character character = CharacterManager.Load();
-            //List<CharacterArmor> characterArmor = CharacterManager.Load();
+
+            //Run Async Task for Loading in CharacterArmor
+            Task.Run(async () =>
+            {
+                var task = await CharacterManager.Load();
+                IEnumerable<Models.CharacterArmor> characterArmors = task;
+                Assert.AreEqual(3, characterArmors.ToList().Count);
+            }).GetAwaiter().GetResult();
+
         }
+
+
     }
 }

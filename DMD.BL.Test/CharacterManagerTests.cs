f546a813f6dda9811e7b41bc75b956bbac075021
Devon Norman <500162063@fvtc.edu>
2022-11-16 12:54:09 -0600

    This file is honestly a huge mess, I think I am doing something wrong.

--- DMD.BL.Test/CharacterManagerTests.cs
+++ DMD.BL.Test/CharacterManagerTests.cs
@@ -19,11 +19,13 @@ namespace DMD.BL.Test
             Task.Run(async () =>
             {
                 var task = await CharacterManager.Load();
-                IEnumerable<Models.Character> characters = task;
+                //IEnumerable<Models.Character> characters = task;
+                List<Models.Character> characters = task;
                 Assert.AreEqual(3, characters.ToList().Count);
             }).GetAwaiter().GetResult();
 
 
+            /* CODE TO TRY AND LOAD CHARACTERARMOR.
             //Run Async Task for Loading in CharacterArmor
             Task.Run(async () =>
             {
@@ -31,9 +33,24 @@ namespace DMD.BL.Test
                 IEnumerable<Models.CharacterArmor> characterArmors = task;
                 Assert.AreEqual(3, characterArmors.ToList().Count);
             }).GetAwaiter().GetResult();
+            */
 
         }
 
 
+        /* THIS IS A MESS, MAY NEED HELP
+        //Test the Ability to Insert Data
+        [TestClass]
+        public void InsertTest()
+        {
+            Task.Run(async () =>
+            {
+                int results = await ModelManager.Insert(new Models.Model { Description = "NewModel" }, true);
+                Assert.IsTrue(results > 0);
+
+            });
+
+        }
+        */
     }
 }
